73d731cc976dff5ae79a289bee2feac2487acc5a
aurel-shael <deshyesguy2@gmail.com>
2024-02-06 13:56:35 +0100

    add function getCaloriesFromInputs

--- script.js
+++ script.js
@@ -36,8 +36,17 @@ function getCaloriesFromInputs(list) {
   let calories = 0;
 
   for (let i = 0; i < list.length; i++) {
+    const currVal = cleanInputString(list[i].value);
+    const invalidInputMatch = isInvalidInput(currVal);
 
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
   }
+  return calories
 }
 
 addEntryButton.addEventListener("click", addEntry);
